733d55a782df2ba8ea99c64d071a71a3506b84f8
Héctor <hmoracobo@hotmail.com>
2026-05-11 13:39:58 +0200

    Inicializa proyecto Spring Boot PepitoBuscaError

---
+++ pepito-busca-error/src/main/java/com/pepitobuscaerror/PepitoBuscaErrorApplication.java
@@ -0,0 +1,13 @@
+package com.pepitobuscaerror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepitoBuscaErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PepitoBuscaErrorApplication.class, args);
+	}
+
+}
---
+++ pepito-busca-error/src/test/java/com/pepitobuscaerror/PepitoBuscaErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pepitobuscaerror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepitoBuscaErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
